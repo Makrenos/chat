ba2822fbb10eb5d7f8b8a63c9a76e2c64ecdf987
Kovács László <kovacs.laszlo@outlook.com>
2020-04-03 22:30:45 +0200

    build

---
+++ build/precache-manifest.4cf3773b053ddf7667f3c3db3ba67d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfcddd4b4fc2230129b884df3e472c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffd70074c223beebfb60",
+    "url": "./static/css/main.bd7da8fd.chunk.css"
+  },
+  {
+    "revision": "6aab35904aa9d09b6511",
+    "url": "./static/js/2.b2da73a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b2da73a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd70074c223beebfb60",
+    "url": "./static/js/main.a0eb810b.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
